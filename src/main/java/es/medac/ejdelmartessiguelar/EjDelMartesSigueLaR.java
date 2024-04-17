80250cde30d3bcddb9d7cf6806a17032e9baa478
mdc-ara <149468225+mdc-ara@users.noreply.github.com>
2024-04-17 13:52:24 +0200

    commit inicial

---
+++ src/main/java/es/medac/ejdelmartessiguelar/EjDelMartesSigueLaR.java
@@ -0,0 +1,13 @@
+
+package es.medac.ejdelmartessiguelar;
+
+/**
+ *
+ * @author asier.ruiz
+ */
+public class EjDelMartesSigueLaR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

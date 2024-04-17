203180e3528a931777233e12b1cfebe244f9653d
mdc-ara <149468225+mdc-ara@users.noreply.github.com>
2024-04-17 14:00:30 +0200

    Métodos method_a y method_b implementados para hacer la combinación de elementos mediante recursividad

--- src/main/java/es/medac/ejdelmartessiguelar/EjDelMartesSigueLaR.java
+++ src/main/java/es/medac/ejdelmartessiguelar/EjDelMartesSigueLaR.java
@@ -8,6 +8,25 @@ package es.medac.ejdelmartessiguelar;
 public class EjDelMartesSigueLaR {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        char[] a_c = {'A','B','C','D','E'};
+        method_a(a_c);
+    }
+
+    private static void method_a(char[] a_c) {
+        for(int s=1;s<=a_c.length;s++){
+            System.out.println("S "+s+":");
+            method_b(a_c,new char[s],0,0);
+        }
+    }
+
+    private static void method_b(char[] a_c, char[] com, int st, int de) {
+        if(de==com.length){
+            System.out.println(new String(com));
+            return;
+        }
+        for(int i=st;i<a_c.length;i++){
+            com[de]=a_c[i];
+            method_b(a_c,com,i+1,de+1);
+        }
     }
 }
